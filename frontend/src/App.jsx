79cc1c78ba41e3669a70d3bea0da632fb48e4101
sunnrooy-stack <sunnrooy@gmail.com>
2026-04-30 13:09:41 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,7 +41,7 @@ import ManagePlaylist from './pages/ManagePlaylist'
 import UseGetRecommendation from './customHooks/UseGetRecommendation'
 import RevenuePage from './component/RevenuePage'
 
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://playtube-1-v4pt.onrender.com"
 
 const ProtectedRoute = ({ userData, children }) => {
   if (!userData) {
