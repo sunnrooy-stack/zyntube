fb3c37060d2902cf0a110d2ab90d833dbdf5388d
sunnrooy-stack <sunnrooy@gmail.com>
2026-05-24 11:51:25 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-   origin:"http://localhost:5173",
+   origin:"https://zyntube-urkw.onrender.com",
    credentials:true
 }))
 
